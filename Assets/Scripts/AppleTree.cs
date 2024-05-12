347a61f712863b92a38db9e4341b66f156587594
Yrii Khwas <yrakhwas@gmail.com>
2024-05-12 17:47:38 +0300

    Create ApplePicker project

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;//template for tree
+
+    public float speed = 1f;//speed for apple tree;
+    public float leftAndRightEdge = 10f; //left and right side of screen;
+    public float chanceToChangeDirections = 0.1f;//імовірність зміни напрямку;
+    public float secondsBetweenAppleDrops = 1f;//interval falling apple;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //getting current position
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if(pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }     
+    }
+    private void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
