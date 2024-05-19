1aca36faaf253f3f9e57a00ece7fb131b001ff28
Yrii Khwas <yrakhwas@gmail.com>
2024-05-19 17:27:42 +0300

    Add score

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBasket = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        basketList = new List<GameObject> ();
+        for (int i = 0; i < numBasket; i++)
+        {
+            GameObject tBasketGo = Instantiate(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY +(basketSpacingY *i);
+            tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void AppleDestroyed()
     {
-        
+        GameObject[] list = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject go in list)
+        {
+            Destroy(go);
+        }
+        int basketIndex = basketList.Count - 1;
+        GameObject gameObject = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(gameObject);
+        if(basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene_0");
+        }
     }
+    
 }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    private void Start()
     {
-        
+        GameObject gameObject = GameObject.Find("ScoreCounter");
+        scoreGT = gameObject.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        if(gameObject.tag == "Apple")
+        {
+            Destroy(gameObject);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+            if(score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+        }
+    }
+    
 }
